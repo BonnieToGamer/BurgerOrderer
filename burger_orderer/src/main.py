11eec71bc410e78e1c72a3db1b15c7cf2a5e1ff4
LeoLarssonSU21 <leo05larsson@gmail.com>
2024-10-10 13:35:26 +0200

    Added Mongodb

--- burger_orderer/src/main.py
+++ burger_orderer/src/main.py
@@ -1,11 +1,31 @@
 from flask import Flask, jsonify, request
-
 import json
+import pymongo
+import os
+
+MONGODB_URL = os.getenv("MONGODB_URL")
+mongo_client = pymongo.MongoClient(MONGODB_URL)
+
+burger_db = mongo_client["burger_db"]
+
+burger_collection = burger_db["burgers"]
 
 app = Flask(__name__)
 
+def burger_exists(burger_name):
+    return burger_collection.count_documents({ 'name': burger_name }, limit = 1) != 0
+
 with open("./hamburgers.json", encoding="utf-8") as json_file:
     parsed_json = json.load(json_file)
+
+for burger_name, burger_details in parsed_json.items():
+    burger = {
+        "name": burger_name,
+        "specials": burger_details["Specials"]
+    }
+
+    if not burger_exists(burger_name):
+        burger_collection.insert_one(burger)
 
 
 @app.route("/")
@@ -19,18 +39,17 @@ def test():
 @app.route("/api/getBurgers")
 def get_burgers():
     burgers = []
-    for burger in parsed_json.keys():
-        burgers.append(burger)
+    for burger in burger_collection.find():
+        burgers.append(burger["name"])
     return jsonify(burgers)
 
 @app.route("/api/getSpecials")
 def get_specials():
     burger_name = request.args.get("burger", default = "", type = str)
-
-    print(burger_name)
-
-    if burger_name in parsed_json:
-        return jsonify(parsed_json[burger_name])
+    
+    if burger_exists(burger_name):
+        for burger in burger_collection.find({"name": burger_name}, limit = 1):
+            return jsonify(burger["specials"])
     else:
         return jsonify({"error": "burger not found"})
 
