4cc83b508be87c509d74e4d2c6dd64d693d33bf7
420k <user1@DESKTOP-8ERG12Q>
2024-10-16 17:10:20 +0200

    Added docstrings for kitchen_view and corrected misspelled folder

--- kitchen_view/src/main.py
+++ kitchen_view/src/main.py
@@ -7,13 +7,21 @@ log.setLevel(logging.ERROR)
 
 app = Flask(__name__)
 
-# why is this just not in the language?
+
 class Order(TypedDict):
     burger: str
     specials: list [str]
 
 
 def print_order(order: Order):
+    """
+    Recieves the order and prints it to the terminal.
+    
+    :param order: An instance of the Order class. It contains the details of the order, the burger type and any special requests
+
+    The function prints the burger type and special requests in a user-friendly format.
+    """
+    
     print("Ny beställning:")
     print("-" * 30)
     print(f"{'Typ av Burger:':<20} {order['burger'].capitalize()}")
@@ -22,6 +30,15 @@ def print_order(order: Order):
 
 @app.route("/newOrder", methods=["POST"])
 def handle_new_order():
+    """
+    Handles a new order request.
+    
+    :return: returns a response which indicates success or error.
+    
+    It takes the order data from the POST request and creates an instance of the Order class. 
+    If any issues occur during processing of the data, it returns an error with the status code 400.
+    If the order process was successful, it prints the order details with print_order(order) and returns a success message with status code 200.
+    """
     try:
         order = request.get_json(force=True)["order"]
         order = Order(order)
